e61ae90d53a1d470b10e2b3ad9a90691fa06861a
王勇 <yong.wang@ly.com>
2024-04-15 18:40:04 +0800

    fix: for code review #236

--- include/logical_plan/query_context.h
+++ include/logical_plan/query_context.h
@@ -48,6 +48,7 @@ struct QueryStat {
     int32_t     sql_length;
     int32_t     region_count;
     bool        hit_cache;
+    bool        hit_query_cache;
     timeval     start_stamp;
     timeval     send_stamp;
     timeval     end_stamp;
@@ -97,6 +98,7 @@ struct QueryStat {
         sql_length          = 0;
         txn_alive_time      = 0;
         hit_cache           = false;
+        hit_query_cache     = false;
         gettimeofday(&(start_stamp), NULL);
         gettimeofday(&(send_stamp), NULL);
         gettimeofday(&(end_stamp), NULL);
