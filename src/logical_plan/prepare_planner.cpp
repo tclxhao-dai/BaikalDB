5312e3ef64d698d3085f38836969dc375bc9b2c8
yuzhong.chen <yuzhong.chen@ly.com>
2024-07-11 17:59:09 +0800

    fix: prepare union set dml txn

--- src/logical_plan/prepare_planner.cpp
+++ src/logical_plan/prepare_planner.cpp
@@ -272,7 +272,9 @@ int PreparePlanner::stmt_execute(const std::string& stmt_name, std::vector<pb::E
     // TODO dml的plan复用
     if (!prepare_ctx->is_select || prepare_ctx->sub_query_plans.size() > 0 || (prepare_ctx->root != nullptr && prepare_ctx->root->has_optimized())) {
         // enable_2pc=true or table has global index need generate txn_id
-        set_dml_txn_state(prepare_ctx->prepared_table_id);
+        if (!prepare_ctx->is_select && prepare_ctx->prepared_table_id != -1) {
+            set_dml_txn_state(prepare_ctx->prepared_table_id);
+        }
         _ctx->plan.CopyFrom(prepare_ctx->plan);
         int ret = _ctx->create_plan_tree();
         if (ret < 0) {
