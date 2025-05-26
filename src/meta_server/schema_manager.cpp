12159ff5b1618de84af492ce21bc1f43dcbe9908
yuzhong.chen <yuzhong.chen@ly.com>
2025-05-26 08:59:09 +0800

    fix: db resource tag

--- src/meta_server/schema_manager.cpp
+++ src/meta_server/schema_manager.cpp
@@ -852,6 +852,7 @@ int SchemaManager::pre_process_for_create_table(const pb::MetaManagerRequest* re
         SET_REQUEST_TABLE_INFO(byte_size_per_record);
         SET_REQUEST_TABLE_INFO(replica_num);
         SET_REQUEST_TABLE_INFO(region_split_lines);
+        SET_REQUEST_TABLE_INFO(resource_tag);
         SET_REQUEST_TABLE_INFO(main_logical_room);
 
     #undef SET_REQUEST_TABLE_INFO
